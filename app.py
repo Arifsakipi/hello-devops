fc9a753d27f895b75361a83b254809f9f9e95bb6
Arifsakipi <arifsakipii@gmail.com>
2026-06-25 10:59:36 +0200

    Initial commit: hello-devops API + Dockerfile

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def hello():
+    return {"message": "Hello from DevOps!"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
